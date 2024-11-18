bc48609eff10784944a0d96fb4016913a849e907
ronaldlehot <ronaldlehot@gmail.com>
2024-11-19 00:36:48 +0800

    fix footer and fix codition in fitur penilaian

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,15 +5,15 @@
                 <h2 class="text-xl font-bold mb-4">SPK Coprass</h2>
                 <p class="text-gray-400">Sistem Pendukung Keputusan Copras adalah sistem yang membantu pengambilan keputusan dengan metode Copras</p>
             </div>
-            <div class="w-full md:w-1/3 mb-6 md:mb-0">
+            {{-- <div class="w-full md:w-1/3 mb-6 md:mb-0">
                 <h2 class="text-xl font-bold mb-4">Navigasi</h2>
-                {{-- <ul>
+                <ul>
                     <li class="mb-2"><a href="/" class="text-gray-400 hover:text-white">Beranda</a></li>
                     <li class="mb-2"><a href="/books" class="text-gray-400 hover:text-white">Buku</a></li>
                     <li class="mb-2"><a href="/categories" class="text-gray-400 hover:text-white">Kategori</a></li>
                     <li class="mb-2"><a href="/mybook" class="text-gray-400 hover:text-white">My Book</a></li>
-                </ul> --}}
-            </div>
+                </ul>
+            </div> --}}
             <div class="w-full md:w-1/3">
                 <h2 class="text-xl font-bold mb-4">Kontak Kami</h2>
                 <p class="text-gray-400 mb-2">Email: info@coprass.com</p>
@@ -26,7 +26,7 @@
             </div>
         </div>
         <div class="mt-8 text-center text-gray-400">
-            &copy; 2023 spk coprass. All rights reserved.
+            &copy; 2024 spk coprass. Ngobar Team.
         </div>
     </div>
 </footer>
